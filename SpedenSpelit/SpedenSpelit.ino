9752b0bf89f9edbaa8187c0d045ff95b27f6478a
NickEZz <Minime.yt@gmai.com>
2024-11-27 18:29:36 +0200

    korjattu nappien rekisteröinti että sama nappi ei rekisteröidy useasti

--- SpedenSpelit/SpedenSpelit.ino
+++ SpedenSpelit/SpedenSpelit.ino
@@ -19,6 +19,10 @@ int randomNumbers[100];  // Satunnainen taulukko pelin arvotuille numeroille
 int userNumbers[100];    // Käyttäjän syöttämille numeroille
 int counter = 0;  // Laskuri, kuinka monta numeroa on arvottu
 int numberCount = 0;  // Laskuri arvottujen numeroiden määrälle
+int userCounter = 0; // Laskuri, käyttäjän painamille numeroille
+
+
+
 unsigned long lastLedChange = 0;
 const unsigned ledChangeTimer = 3000; // 3 sec
 int score = 0;
@@ -42,23 +46,18 @@ void loop()
      Serial.print("nappi 4");
       buttonNumber = -1;
     }
-      if (buttonNumber == 1){
-      Serial.print("nappi 1");
-      buttonNumber = -1;
+ if (buttonNumber >= 0) {
+    if (buttonNumber == 4) {
+        startTheGame();
+    } else if (isGameRunning && userCounter < 100) {
+        userNumbers[userCounter] = buttonNumber;
+        Serial.print("Käyttäjän numero: ");
+        Serial.println(userNumbers[userCounter]);
+        userCounter++;
     }
-       if (buttonNumber == 2){
-      Serial.print("nappi 2");
-      buttonNumber = -1;
-    }
-     
-       if (buttonNumber == 3){
-      Serial.print("nappi 3");
-      buttonNumber = -1;
-    }
-   if (buttonNumber == 0){
-      Serial.print("nappi 0");
-      buttonNumber = -1;
-    }
+    buttonNumber = -1; // Nollaa painikemuuttuja välittömästi
+}
+
    // Jos timeri keskeyttää ja uusi numero on arvottu
   if (newTimerInterrupt) {
    
@@ -70,10 +69,10 @@ void loop()
            do {
            randomNumbers[counter] = random(0, 4);  // Arvotaan numero 0-3
          } while (counter > 0 && randomNumbers[counter] == randomNumbers[counter - 1]);
-           Serial.print("Arvottu numero: ");
+        //   Serial.print("Arvottu numero: ");
          showResult(randomNumbers[counter]);
          setLed(randomNumbers[counter]);
-         Serial.println(randomNumbers[counter]);
+         //  Serial.println(randomNumbers[counter]);
                                 // Tulostetaan numero sarjamonitoriin
          
        }
@@ -125,9 +124,10 @@ void initializeTimer(void){
 ISR(TIMER1_COMPA_vect)
 {
    newTimerInterrupt = true; 
-     if (isGameRunning) {
-        newTimerInterrupt = true;
-    }
+    // if (isGameRunning) {
+   //     newTimerInterrupt = true;
+      
+    //}
    
 }
 
@@ -151,6 +151,12 @@ void initializeGame()
 void startTheGame()
 { 
    isGameRunning = true;  
+   kerroin = 1; // Nollataan kerroin
+   counter = 0; // Nollataan laskuri
+   userCounter = 0; // Nollataan käyttäjän laskuri
+    for (int i = 0; i < 100; i++) {
+        userNumbers[i] = -1; // Nollataan käyttäjän numerot
+    }
   Serial.println("Peli aloitettu");
    // see requirements for the function from SpedenSpelit.h
 }
@@ -161,7 +167,9 @@ void stopTheGame()
     counter = 0;
     numberCount = 0;
     kerroin = 1; // Palautetaan alkuperäinen nopeus
+    userCounter = 0; // Nollataan käyttäjän laskuri
     updateOCR1A(); // Päivitetään keskeytys
+    clearAllLeds();
     isGameRunning = false;
    // see requirements for the function from SpedenSpelit.h
 }
--- SpedenSpelit/buttons.cpp
+++ SpedenSpelit/buttons.cpp
@@ -3,32 +3,40 @@
 //volatile int buttonNumber = -1;
 volatile bool btnpress = false;
 static unsigned long lastbouncetime = 0;
-const unsigned long bouncedelay = 100;
+const unsigned long bouncedelay = 250;
+
+unsigned long lastPressTime[5];  // Aikaa viimeisimmän napin painalluksen rekisteröimiseksi (5 nappia, pin 2-6)
+
 
 extern byte currentLed;
 
 void initButtonsAndButtonInterrupts(void)
-{
-  PCICR |= (1 << PCIE2); 
-  PCMSK2 = 0b01111100;
+{   // Aktivoi keskeytykset PCINT2 (painikkeet 2-6)
+  PCICR |= (1 << PCIE2);      // PCIE2 mahdollistaa PCINT-sarjan keskeytykset
+  PCMSK2 = 0b01111100;      // Vain pin 2-6 aktivoidaan (pin-maski)
+
+
   // See requirements for this function from buttons.h
   for(int napit = 2;napit<7;napit++)
   {
     pinMode(napit, INPUT_PULLUP);
   }
 }
-
+// Keskeytyksellinen napin rekisteröinti
 ISR(PCINT2_vect) {
-  static unsigned long lastInterruptTime = 0;
-  unsigned long currentTime = millis();
-  if (currentTime - lastInterruptTime) {
+    byte pinState = PIND & 0b01111100; // Lue painikeportti (vain 2-6)
+
     for (int button = 2; button < 7; button++) {
-      if (digitalRead(button) == LOW) {
-        buttonNumber = button -2 ;
-       
-        break;
-      }
+        if (!(pinState & (1 << button))) { // Tarkista, onko nappi painettu alas
+            unsigned long currentTime = millis();  // Hae nykyinen aika
+            int buttonIndex = button - 2;  // Lasketaan nappi-indeksi (0-4)
+
+            // Varmistetaan, että viivettä on kulunut tarpeeksi ennen uutta rekisteröintiä
+            if (currentTime - lastPressTime[buttonIndex] >= bouncedelay) {
+                buttonNumber = buttonIndex;  // Aseta rekisteröity nappi
+                lastPressTime[buttonIndex] = currentTime;  // Tallenna tämän napin rekisteröintiaika
+                break; 
+            }
+        }
     }
-  }
-  lastInterruptTime = currentTime;
 }
