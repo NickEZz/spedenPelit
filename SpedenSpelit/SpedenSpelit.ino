3a9ae6e2df333f774a9fb9a1b11c78347e78ab11
NickEZz <Minime.yt@gmai.com>
2024-12-11 15:15:03 +0200

    koodin siistimistä ja lisätty ledien vilkkuminen lisätty kommentteja
    high score featurea parannettu

--- SpedenSpelit/SpedenSpelit.ino
+++ SpedenSpelit/SpedenSpelit.ino
@@ -8,68 +8,48 @@
 // loop() function and interrupt handlers
 volatile int buttonNumber = -1;           // for buttons interrupt handler
 volatile bool newTimerInterrupt = false;  // for timer interrupt handler
-
 volatile bool isGameRunning = false;
 unsigned long previousMillis = 0; // Aikaseuranta
 float kerroin = 1; 
 unsigned long x = 0;
-const unsigned long y = 700;
-
 int randomNumbers[100];  // Satunnainen taulukko pelin arvotuille numeroille
 int userNumbers[100];    // Käyttäjän syöttämille numeroille
 int counter = 0;  // Laskuri, kuinka monta numeroa on arvottu
 int numberCount = 0;  // Laskuri arvottujen numeroiden määrälle
 int userCounter = 0; // Laskuri, käyttäjän painamille numeroille
-
-
-unsigned long lastLedChange = 0;
-const unsigned ledChangeTimer = 3000; // 3 sec
-byte score = 0;
-byte highScore = 0;
+byte score = 0; //pisteiden muuttuja
+byte highScore = 0; //highscore muuttuja
 byte currentLed = -1;
-bool ledChanged = false;
 
 
 void setup(){
-  delay(500); //vakaatuminen
+  delay(500); //0.5 sekunnin viive 
   Serial.begin(9600);  // Aloitetaan sarjayhteys 9600 baudilla
-  initializeTimer();
-  initializeDisplay();
-
-
+  initializeTimer(); 
+  initializeDisplay(); //näyttön koodit päälle
   isGameRunning = false;
-  initializeLeds();
+  initializeLeds();  //ledienkoodit päälle
   initButtonsAndButtonInterrupts(); // suorittaa nappien inilizaation
-  interrupts();
-
+  interrupts(); //interruptit päälle
   // EEPROM
-  highScore = loadHighScore();
-  Serial.println(highScore);
+  highScore = loadHighScore(); //ladataan highscore näytölel
+  showResult(highScore); //ladataan highscore näytölel
+  
 }
 
 void loop()
 {
-  
-  if(!isGameRunning){
-      
- //   Serial.println("korkein score on:");
-//    Serial.println(highScore);
-    showResult(highScore);
-  
-  }
 
  if (buttonNumber >= 0) {// jos buttonNumber on yli 0 suoritetaan if lause
     if (buttonNumber == 4) {//jos ButtonNumber on 4 eli painetaan nappia 6 niin aloitetaan peli
         startTheGame();
     } else if (isGameRunning && userCounter < 100) {
         userNumbers[userCounter] = buttonNumber;//tallentaa napin painalluksen taulukkoon
-        Serial.print("Käyttäjän numero: ");
-        Serial.println(userNumbers[userCounter]);
         userCounter++;//menee taulukossa yhden eteenpäin
         checkGame(userNumbers[userCounter - 1]);
     }
     buttonNumber = -1; // Nollaa painikemuuttuja välittömästi
-}
+  }
    // Jos timeri keskeyttää ja uusi numero on arvottu
   if (newTimerInterrupt) {
    
@@ -83,33 +63,27 @@ void loop()
          } while (counter > 0 && randomNumbers[counter] == randomNumbers[counter - 1]);
          
          setLed(randomNumbers[counter]);
-         Serial.println(randomNumbers[counter]);
       numberCount ++;
        }
      // Päivitetään laskuri ja tarkistetaan, onko 10 numeroa arvottu
       counter++;   
-      
-
-     if (numberCount == 10) {
-      numberCount = 0;
+     if (numberCount == 10) { //jos numero laskut on 10
+      numberCount = 0;        //numero laskuri takaisin 0
       kerroin = kerroin * 0.9;  // Vähennetään aikaväliä 10 %
-
       // Päivitetään timerin väli (keskeytyksen aika)
-       updateOCR1A();
-      Serial.print("Nopeus kasvoi! Uusi aikaväli: ");
-      Serial.println(kerroin);
+       updateOCR1A();//päivitetään ocr1a
      }
-     if (score == 99){
+     if (score == 99){ //jos score on 99
       highScore = score;
-      stopTheGame();
-    }
+      stopTheGame(); //pysäytetään peli
+      }
     }
 }
-
 void initializeTimer(void){
-  TCCR1A = 0;
+  TCCR1A = 0; //nollataan timer reksiteri
+  TCNT1 = 0;  //nollaa timerin
   TCCR1B = (1 << WGM12) | (1 << CS12) | (1 << CS10);  // CTC-tila, prescaler 1024
-  updateOCR1A();
+  updateOCR1A(); //päivitetään ocr1a
   TIMSK1 = (1 << OCIE1A);  // Oikea keskeytys asetettu
   sei();  // Ota keskeytykset käyttöön
  
@@ -126,32 +100,26 @@ void checkGame(byte nbrOfButtonPush)
 	 if (userCounter > 0 && userNumbers[userCounter - 1] == randomNumbers[userCounter - 1]) {
         // Jos käyttäjän syöttämä numero on oikea
         score++;  // Lisää pisteitä
-        Serial.print("Oikea numero! Pisteet: ");
-        Serial.println(score);
-        showResult(score);
-
-        // Voit halutessasi tehdä muita toimenpiteitä, kuten ledin vilkuttaminen
-       
+        showResult(score); //näytä score näytöllä 
     } else {
-        // Jos numerot eivät täsmää
-      
-        stopTheGame();
-        
+        //jos numerot ei täsmää
+        stopTheGame(); // lopeta peli
     }
 }
 void startTheGame()
 { 
-  delay(100);
-   isGameRunning = true;  
-   score = 0;
+  showResult(highScore);
+  ledVilkutus();
+  showResult(0); //näytölle näkymään 0 
+  initializeTimer();
+   isGameRunning = true;  //asetetaan pelipäälle
+   score = 0;   //Nollataan pisteet
    kerroin = 1; // Nollataan kerroin
    counter = 0; // Nollataan laskuri
    userCounter = 0; // Nollataan käyttäjän laskuri
     for (int i = 0; i < 100; i++) {
         userNumbers[i] = -1; // Nollataan käyttäjän numerot
     }
-  Serial.println("Peli aloitettu");
-   // see requirements for the function from SpedenSpelit.h
 }
 void stopTheGame()
 {
@@ -159,28 +127,38 @@ void stopTheGame()
         highScore = score;
         saveHighScore(highScore);
     }   
-   
-    Serial.println("Peli pysäytetty");
-    counter = 0;
-    numberCount = 0;
+    counter = 0; //nollataan lasku
+    numberCount = 0; //nollataan numeroiden lasku
     kerroin = 1; // Palautetaan alkuperäinen nopeus
     userCounter = 0; // Nollataan käyttäjän laskuri
     updateOCR1A(); // Päivitetään keskeytys
-    clearAllLeds();
-    setAllLeds();
-    isGameRunning = false;
-   // see requirements for the function from SpedenSpelit.h
+    clearAllLeds(); //ledit pois päältä
+    setAllLeds();   //kaikki ledit päälle
+    isGameRunning = false; //peli pois päältä
 }
 void saveHighScore(byte highScore){
-  Serial.print("Tallennetaan EEPROM:iin: ");
-  Serial.println(highScore);
   EEPROM.update(0, highScore);// Voi valita kumpaa käyttää, update vois olla parempi
  // Päivitä muuttuja jos arvo muuttunut
   //EEPROM.put(0, highScore); // Tallenetaan highScore 0 osotteeseen
 }
 byte loadHighScore() {
-  byte storedHighScore = EEPROM.read(0); 
-  Serial.print("Ladattu EEPROM:sta: ");
-  Serial.println(storedHighScore);
-  return storedHighScore;
+  byte storedHighScore = EEPROM.read(0);  //luetaan high score eepromista
+  return storedHighScore; //palautetaan
 }
+void ledVilkutus(){
+  showResult(highScore);
+       delay(500);   //0.5s tauko
+  setAllLeds(); //ledit päälle
+       delay(500); //0.5s tauko
+  clearAllLeds(); //ledit pois päältä
+       delay(500); //0.5s tauko
+  setAllLeds(); //ledit päälle
+       delay(500); //0.5s tauko
+  clearAllLeds(); //ledit pois päältä
+       delay(500); //0.5s tauko
+  setAllLeds(); //ledit päälle
+       delay(500); //0.5s tauko
+  clearAllLeds();  //ledit pois päältä
+       delay(500); //0.5s tauko
+  showResult(0); //näytölle näkymään 0 
+  }
